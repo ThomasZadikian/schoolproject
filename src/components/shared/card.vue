576ed018e86dd83f4c5febf190745c1d902f028d
ThomasZadikian <thomas.zadikian@businessdecision.com>
2024-04-23 08:34:52 +0200

    add card components

---
+++ src/components/shared/card.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const props = defineProps<{
+	title: string
+	subtitle: string
+	content: string
+}>()
+</script>
+
+<template>
+	<v-card border="sm" width="30%" height="450" :hover="true">
+		<v-card-title>{{ props.title }}</v-card-title>
+		<v-card-subtitle>{{ props.subtitle }}</v-card-subtitle>
+		<v-card-text>{{ props.content }}</v-card-text>
+	</v-card>
+</template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,10 +1,22 @@
 <script setup lang="ts">
 import { useI18n } from 'vue-i18n'
 import searchBar from '@/components/shared/searchBar.vue'
+import card from '@/components/shared/card.vue'
 
 const { t } = useI18n()
+// Des stores (pinia) remplaceront fakeData
+const fakeData = {
+	card1: {
+		title: 'Je suis la carte 01',
+		subtitle: 'Je suis le sous titre de la carte 01',
+		content: 'Je suis le contenu de cette carte',
+	},
+}
 </script>
 
 <template>
 	<searchBar></searchBar>
+	<container v-for="data in fakeData">
+		<card :title="data.title" :subtitle="data.subtitle" :content="data.content"></card>
+	</container>
 </template>
