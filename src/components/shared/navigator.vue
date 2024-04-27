f2bbc62ec4097acb808470011a65811061ef0422
Thomas <Thoma.zadikian@gmail.com>
2024-04-27 14:42:54 +0200

    initialse the navigator

---
+++ src/components/shared/navigator.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts"></script>
+
+<template>
+    <v-bottom-navigation elevation="5" rounded="xl" >
+  <v-btn value="recent">
+    <v-icon>mdi-history</v-icon>
+
+    <span>Recent</span>
+  </v-btn>
+
+  <v-btn value="favorites">
+    <v-icon>mdi-heart</v-icon>
+
+    <span>Favorites</span>
+  </v-btn>
+
+  <v-btn value="nearby">
+    <v-icon>mdi-map-marker</v-icon>
+
+    <span>Nearby</span>
+  </v-btn>
+</v-bottom-navigation>
+</template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,7 +2,7 @@
 import { useI18n } from 'vue-i18n'
 import searchBar from '@/components/shared/searchBar.vue'
 import card from '@/components/shared/card.vue'
-
+import navigator from '@/components/shared/navigator.vue'
 const { t } = useI18n()
 // Des stores (pinia) remplaceront fakeData
 const fakeData = {
@@ -50,4 +50,5 @@ const fakeData = {
 			<!-- Un onClick redirigera vers la page de détail de la carte -->
 			<card v-for="data in fakeData" :title="data.title" :subtitle="data.subtitle" :content="data.content"></card>
 	</div>
+	<navigator></navigator>
 </template>
