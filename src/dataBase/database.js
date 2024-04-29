d015c9f9c6b951b8bd1c49f304ae779100f93a2c
ThomasZadikian <thomas.zadikian@businessdecision.com>
2024-04-29 15:18:58 +0200

    create database and add sqlite3

---
+++ src/dataBase/database.js
@@ -0,0 +1,15 @@
+const sqlite3 = require('sqlite3').verbose()
+
+let db = new sqlite3.Database(
+	'./my_database.db',
+	sqlite3.OPEN_READWRITE | sqlite3.OPEN_CREATE,
+	(err) => {
+		if (err) {
+			console.error(err.message)
+		} else {
+			console.log('Connected to the database')
+		}
+	},
+)
+
+module.exports = db
