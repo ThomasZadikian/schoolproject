59d9bd35040d2031e43deda43d694aec618d0c1a
ThomasZadikian <thomas.zadikian@businessdecision.com>
2024-04-29 14:31:21 +0200

    add value in fr.json & translate for en lang

--- src/components/shared/navigator.vue
+++ src/components/shared/navigator.vue
@@ -1,30 +1,33 @@
 <script setup lang="ts">
 import colorsEnum from '@/core/types/enums/colorsEnum'
+import { useI18n } from 'vue-i18n'
+
+const { t } = useI18n()
 </script>
 
 <template>
 	<v-bottom-navigation elevation="5" :bg-color="colorsEnum.BG_DARK" :grow="true" class="blur-bg">
-		<v-btn value="Accueil">
+		<v-btn :value="t('navigator.home')">
 			<!-- L'accueil est le lieu où tout les portfolio apraissent -->
 			<v-icon>mdi-history</v-icon>
 
-			<span>Accueil</span>
+			<span>{{ t('navigator.home') }}</span>
 		</v-btn>
 
-		<v-btn value="Portfolio">
+		<v-btn :value="t('navigator.portfolio')">
 			<!-- Affiche le portfolio de l'utilisateur -->
 
 			<v-icon>mdi-pencil</v-icon>
 
-			<span>Portfolio</span>
+			<span>{{ t('navigator.portfolio') }}</span>
 		</v-btn>
 
-		<v-btn value="Vidéo">
+		<v-btn :value="t('navigator.video')">
 			<!-- Affiche le portfolio de l'utilisateur -->
 
 			<v-icon>mdi-movie</v-icon>
 
-			<span>Vidéo</span>
+			<span>{{ t('navigator.home') }}</span>
 		</v-btn>
 	</v-bottom-navigation>
 </template>
